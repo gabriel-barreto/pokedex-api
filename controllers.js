3ab84eb69b13c14ace0897ab963f8f616c10fae9
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 14:48:18 -0300

    Remove trailing spaces

--- controllers.js
+++ controllers.js
@@ -1,49 +1,39 @@
-const pokedex = require('./functions');
+const pokedex = require("./functions");
 
 const list = async (req, res) => {
     try {
         const pokemons = await pokedex.getPokemons();
-        res
-            .status(200)
-            .send({
-                success: true,
-                content: pokemons
-            });
-    }
-    catch(err) {
+        res.status(200).send({
+            success: true,
+            content: pokemons
+        });
+    } catch (err) {
         console.log(err);
-        res
-            .status(500)
-            .send({
-                success: false,
-                message: err.message,
-            });
+        res.status(500).send({
+            success: false,
+            message: err.message
+        });
     }
-}
+};
 
 const details = async (req, res) => {
     const { id } = req.params;
-    
     try {
         const pokemon = await pokedex.getPokemon(id);
         if (pokemon) {
-            res
-                .status(200)
-                .send({ success: true, content: pokemon });
+            res.status(200).send({ success: true, content: pokemon });
         } else {
-            res
-                .status(404)
-                .send({ success: false, message: 'Pokemon not found in 1st generation' });
-        }
-    }
-    catch(err) {
-        console.log(err);
-        res.
-            status(500)
-            .send({
+            res.status(404).send({
                 success: false,
-                message: err.message,
+                message: "Pokemon not found in 1st generation"
             });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: err.message
+        });
     }
 };
 
